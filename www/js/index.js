ed616e19a9c8a672e3030a5b7efc6f38b251a696
aksanoble <aksanoble@gmail.com>
2015-03-29 20:09:35 +0530

    change plugin version

--- www/js/index.js
+++ www/js/index.js
@@ -1,47 +1,3 @@
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-        var testElement = document.getElementById('test');
-        testElement.innerHTML = 'That';
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-        window.addEventListener("batterystatus", onBatteryStatus, false);
-
-        var testElement = document.getElementById('test');
-        testElment.innerHTML = "Ash";
-        function onBatteryStatus(info) {
-    // Handle the online event
-        testElement.innerHTML = info.level;
-      };
-
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-
-};
-
 var addToStatus = function(message) {
 console.log(message);
 var newP = document.createElement("p");
@@ -54,10 +10,8 @@ status.insertBefore(newP, status.firstChild);
 
 var test = {
 addNetwork: function() {
-
-//var ssid =  document.getElementById("SSID").value;
-//var pass = document.getElementById("password").value;
-
+var ssid =  document.getElementById("SSID").value;
+var pass = document.getElementById("password").value;
 var wifiConfiguration = WifiWizard.formatWifiConfig(ssid, pass, 'WPA');
 
 
@@ -87,7 +41,7 @@ scan: function () {
   WifiWizard.getScanResults(listHandler);
   function listHandler(list) {
   addToStatus(list);
-}
+};
 
 },
 
